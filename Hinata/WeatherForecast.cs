acac15933624da566dab9cb0c1d2eb813a6d377b
Adrian Helmchen <adrian.helmchen@outlook.de>
2024-02-25 15:25:54 +0100

    feat(Hinata): change something

--- Hinata/WeatherForecast.cs
+++ Hinata/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace Hinata
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / (5 / 9));
 
         public string? Summary { get; set; }
     }
