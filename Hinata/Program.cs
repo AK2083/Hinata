8e195f8735e8e68760e812ed4759ea719fcaa54e
Adrian Helmchen <adrian.helmchen@outlook.de>
2024-02-29 17:10:07 +0100

    fix(Hinata): change something

--- Hinata/Program.cs
+++ Hinata/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.ClearProviders(); // Entfernt alle standardmäßigen Logging-Provider
+builder.Logging.AddConsole(); // Fügt den Konsolen-Logging-Provider hinzu
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.Logger.LogInformation("Das Passwort ist" + builder.Configuration["ConnectionString"]);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
