cadcaa7b8fe91d0588e0200984cf61b0098d8751
Adrian Helmchen <adrian.helmchen@outlook.de>
2024-02-29 17:26:39 +0100

    fix(Hinata): add connection

--- Hinata/Program.cs
+++ Hinata/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +13,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.Logger.LogInformation("Das Passwort ist" + builder.Configuration["ConnectionString"]);
+try
+{
+    using (var conn = new SqlConnection())
+    {
+        conn.Open();
+        app.Logger.LogInformation("DB erfolgreich");
+    }
+}
+catch (Exception ex)
+{
+    app.Logger.LogError($"Fehler beim Herstellen einer Datenbankverbindung: {ex.Message}");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
